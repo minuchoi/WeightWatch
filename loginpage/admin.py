475507506a6a6b639af468413ee19c681cd3af25
Minu <minuphilipchoi@gmail.com>
2020-11-17 17:07:47 +0000

    Created models for users/passwords and the admin page for the model. (Testing)

--- loginpage/admin.py
+++ loginpage/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import UserDetail
+
+
+@admin.register(UserDetail)
+class UserDetailsAdmin(admin.ModelAdmin):
+    list_display = ['username', 'password']
--- loginpage/views.py
+++ loginpage/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+import os.path
+
+from .models import UserDetail
 
 
 # Create your views here.
 def displaylogin(request):
-    return render(request, 'loginpage/loginpage.html')
+    user_info = UserDetail.objects.all()
+    return render(request, 'loginpage/loginpage.html', {
+        'user_info': user_info
+    })
