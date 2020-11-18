80cc663ce85279dc87948c0c3ef72b9af20e96ac
Minu <minuphilipchoi@gmail.com>
2020-11-18 01:39:55 +0000

    Started work on the weight input page. Created the models and the admin page so far.

--- weightpage/admin.py
+++ weightpage/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Weight
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username']
+
+
+@admin.register(Weight)
+class Weight(admin.ModelAdmin):
+    list_display = ['user', 'weight', 'time']
+
+
+
+
--- weightpage/models.py
+++ weightpage/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.username
+
+
+class Weight(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    weight = models.FloatField(null=False)
+    time = models.DateTimeField()
+
+
+
+
+
