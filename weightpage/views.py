05fffac6d008afc59910295cca8748bc2e75e946
Minu <minuphilipchoi@gmail.com>
2020-11-19 02:15:25 +0000

    Testing out data extraction from database to view.

--- weightpage/views.py
+++ weightpage/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
+from .models import Weight
+
 
 # Create your views here.
 def displayweight(request):
-    return render(request,'weightpage/weightpage.html')
+    list_of_past_weight = Weight.objects.all()
+    output = [w.weight for w in list_of_past_weight]
+    return render(request, 'weightpage/weightpage.html', {
+        'weights': output
+    })
